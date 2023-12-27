d4ea36ae68a003aae865427217a314ea482bb124
Dinu <dinu@mediapark.com>
2023-12-27 11:39:06 +0200

    configure prettier with tw

--- App.js
+++ App.js
@@ -7,10 +7,10 @@ NativeWindStyleSheet.setOutput({
 })
 export default function App() {
   return (
-    <View tw="flex-1 items-center justify-center">
-      {/* <View style={styles.container}> */}
+    <View tw="flex-1 items-center justify-center hover:bg-black hover:opacity-60">
+      <View tw="" />
       <Text className="text-red-600">Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <StatusBar style="auto" /> */}
     </View>
   )
 }
