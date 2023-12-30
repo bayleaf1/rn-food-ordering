ce5f24736a6fc635fdc407f24b1edb6d146998a1
Dinu <dinu@mediapark.com>
2023-12-30 18:25:41 +0200

    add custom navigator

--- App.js
+++ App.js
@@ -1,134 +1,111 @@
-import { StatusBar } from 'expo-status-bar'
-import { Text, View, Dimensions, AppState, LogBox, TextInput } from 'react-native'
-import { NativeWindStyleSheet } from 'nativewind'
-import HomeIcon from './assets/icons/home'
-import LocalPicture from './src/components/Pictures/LocalPicture'
-import RemotePicture from './src/components/Pictures/RemotePicture'
+import 'react-native-gesture-handler'
+import { Animated, Pressable, Text, View } from 'react-native'
+import 'react-native-gesture-handler'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import BaseLayout from './src/layouts/BaseLayout'
 import FontsProvider from './src/providers/FontsProvider'
 // import * as ScreenOrientation from 'expo-screen-orientation'
-import { useEffect, useState } from 'react'
-import Writing from './src/components/Writing'
-import clsx from 'clsx'
-import WritingInput from './src/components/FormRelated/WritingInput'
-// LogBox.ignoreLogs(['`new NativeEventEmitter()` was called with a non-null']) //Hide warnings
 
-// let o = console.warn.bind(console);
+import {
+  NavigationContainer,
+  StackRouter,
+  createNavigatorFactory,
+  useNavigationBuilder,
+} from '@react-navigation/native'
+import React, { useState } from 'react'
+// import {} form '@react-navigation/'
 
-// console.warn = (...props)=>{
-//     // console.log("wARN", ...props);
-//     o(...props, ...props)
-
-// }
-// console.warn("DO WARN")
-// let original = console.log
-// console.log = (...props)=>{
-//   // console.error('AAA', props);
-//   original(...props, ...props)
-// }
-// console.warn("SOME BAD")
-// console.ignoredYellowBox
-// console.warn = () => ''
-// console.disableYellowBox = true;
-// Object.getPrototypeOf(console).warn = () => {
-//   console.log('WARNN')
-// }
-// LogBox.ignoreAllLogs();//Hide all warning notifications on front-end
+import { StackView, createStackNavigator } from '@react-navigation/stack'
+import { NativeWindStyleSheet } from 'nativewind'
+import { createStackNavigatorWithDefaultScreenOptions } from './src/libs/Navigators'
 NativeWindStyleSheet.setOutput({
   default: 'native',
 })
+// LogBox.ignoreLogs(['`new NativeEventEmitter()` was called with a non-null']) //Hide warnings
+
+//TODO if navigation doesn't work on android
+// react-native-screens package requires one additional configuration step to properly work on Android devices. Edit MainActivity.kt or MainActivity.java file which is located under android/app/src/main/java/<your package name>/.
+
+// Add the highlighted code to the body of MainActivity class:
 
 //tw - tailwind
 //ctw - component tailwind;
 
 //TODO de incercat https://github.com/kristerkari/react-native-svg-example?tab=readme-ov-file
-//TODO text, text name component?, textinput, navigation, drawer 1 or 2
+//TODO text, text name component?, textinput, navigation, drawer 1 or 2, translating
+
+const Stack = createStackNavigatorWithDefaultScreenOptions()
+
+function Home({ navigation }) {
+  return (
+    <View tw="mt-20 flex-1 bg-blue-400">
+      <Pressable onPress={() => navigation.navigate('Log')}>
+        <Text tw="bg-white">HOMEx</Text>
+      </Pressable>
+    </View>
+  )
+}
+function Log({ navigation }) {
+  return (
+    <View tw="flex-1 bg-red-400">
+      <Pressable onPress={() => navigation.goBack('Home')}>
+        {/* <Text tw="bg-white">Log</Text> */}
+        <Text tw="bg-white">Log</Text>
+        <Text tw="bg-white">Log</Text>
+        <Text tw="bg-white">Log</Text>
+        <Text tw="bg-white">Log</Text>
+      </Pressable>
+    </View>
+  )
+}
 
 export default function App() {
   let data = Array.from({ length: 200 })
   let link = 'https://i.pinimg.com/originals/82/c6/5b/82c65b9bb0a75026fc4c82a438b4cc9b.jpg'
 
   const [number, onChangeNumber] = useState('')
+
   return (
     <FontsProvider>
       <SafeAreaProvider>
-        <BaseLayout>
-          <View tw="mt-20">
-            <Writing h1 semibold numberOfLines={3} ctw="text-white">
-              Worem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem
-              ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
-            </Writing>
-          </View>
-          {/* CONTINUE */}
-          {/* <WritingInput label="Trlalal"/> */}
-          {/* <WritingInput containerTw="mt-4" /> */}
-          {/* <TextInput
-            onChangeText={(e) => {
-             console.log(typeof e, e)
-              onChangeNumber(e)
-            }}
-            // inputMode="numeric"
-            // secureTextEntry={true}
-            // keyboardType='default'
-            value={number}
-            tw="h-10 text-lg rounded-md border bg-white px-2"
-            style={{ fontFamily: 'Primary' }}
-          ></TextInput> */}
-          {/* <FlatList
-          data={data}
-          renderItem={(i) => <View tw="mr-8 h-20 w-[200px] bg-orange-600" key={i.index} />}
-          ItemSeparatorComponent={<View tw="w-4 h-20 bg-slate-600"></View>}
-          // snapToAlignment="center"
-          // decelerationRate="normal"
-          // stickyHeaderIndices={[0, 1]}
-          // pagingEnabled={true}
-          horizontal
-          // showsHorizontalScrollIndicator={false}
-        >
-          </FlatList> */}
-          {/* {Array.from({ length: 200 }).map((i, key) => (
-            <View tw="w-[200px] mr-8 h-20 bg-orange-600" key={key}/>
-          ))} */}
-
-          {/* <View tw="box-border mb-20  bg-red-400">
-          <Text>
-            Lallalalallalalalallalallalalallalallal
-          </Text>
-        </View>
-
-        <View tw="box-border bg-red-400">
-          <Text>
-            Lallalalallalalalallalallalalallalallalallalalallalal allalallalalallalalallalalal
-          </Text>
-        </View>
-
-        <View tw="box-border border bg-red-400">
-          <Text>
-            Lallalalallalalalallalallalalallalallalallalalallalalallalallalalallalalallalalal
-          </Text>
-        </View> */}
-
-          {/* <View tw="mt-40 h-20 w-20 border"> */}
-          {/* <LocalPicture image="home" /> */}
-          {/* <RemotePicture src={link} ctw="w-1/2 h-1/2" /> */}
-          {/* </View> */}
-          {/* <View tw="w-20 h-20 bg-blue-400" /> */}
-          {/* <View tw="w-20 h-20 bg-green-400" /> */}
-
-          <Text tw="text-white">Open up App.js to start working on your app!</Text>
-          {/* <StatusBar style="auto" /> */}
-          {/* </View> */}
-
-          {/* <Words></Words>
-        <Writing h2 bold > Text value</Writing>
-        <Narrative></Narrative>
-        <AppText></AppText>
-        <Letters></Letters>
-        <Wording></Wording>
-        <Paragraph></Paragraph>
-        <Typhography></Typhography> */}
-        </BaseLayout>
+        <NavigationContainer>
+          <BaseLayout>
+            <Stack.Navigator
+              defaultOptions={{ a: 'b' }}
+              screenOptions={{
+                headerMode: 'float',
+              }}
+            >
+              <Stack.Screen
+                name="Home"
+                component={Home}
+                // options={
+                //   {
+                //     // header: () => null,
+                //     // gestureEnabled: true,
+                //     // gestureDirection: 'horizontal',
+                //     // transitionSpec: {
+                //     //   open: config,
+                //     //   close: config,
+                //     // },
+                //     // // headerMode: 'screen',
+                //     // cardStyleInterpolator: forSlide,
+                //     // HeaderStyleInterpolators: HeaderStyleInterpolators.forFade,
+                //     // HeaderStyleInterpolators
+                //   }
+                // }
+              />
+              <Stack.Screen
+                name="Log"
+                component={Log}
+                options={{
+                  gestureEnabled: true,
+                  gestureDirection: 'vertical',
+                }}
+              />
+            </Stack.Navigator>
+          </BaseLayout>
+        </NavigationContainer>
       </SafeAreaProvider>
     </FontsProvider>
   )
---
+++ src/libs/Navigators.js
@@ -0,0 +1,95 @@
+import * as React from 'react'
+import { useNavigationBuilder, createNavigatorFactory, StackRouter } from '@react-navigation/native'
+import { StackView } from '@react-navigation/stack'
+import { Animated } from 'react-native'
+
+//Changing the file with custom navigator causes freeze of the app and it needs to be reloaded
+
+const config = {
+  animation: 'spring',
+  config: {
+    stiffness: 800,
+    damping: 1000,
+    mass: 1,
+    overshootClamping: false,
+    restDisplacementThreshold: 0.01,
+    restSpeedThreshold: 0.01,
+  },
+}
+
+const defaultOptions = {
+  cardStyleInterpolator: forSlide,
+  transitionSpec: {
+    open: config,
+    close: config,
+  },
+}
+
+function StackNavigator({ initialRouteName, children, screenOptions, ...rest }) {
+  const { state, descriptors, navigation, NavigationContent } = useNavigationBuilder(StackRouter, {
+    children: addDefaultOptionsToScreens(children, defaultOptions),
+    initialRouteName,
+    screenOptions,
+  })
+
+  return (
+    <NavigationContent>
+      <StackView {...rest} state={state} navigation={navigation} descriptors={descriptors} />
+    </NavigationContent>
+  )
+}
+
+function addDefaultOptionsToScreens(children, defaultOptions) {
+  let array = React.Children.toArray(children)
+
+  return array.map((possibleScreen) => {
+    let result = possibleScreen
+
+    if (possibleScreen.type.name === 'Screen') {
+      result = React.cloneElement(possibleScreen, {
+        ...possibleScreen.props,
+        options: { ...defaultOptions, ...(possibleScreen.props.options || {}) },
+      })
+    }
+    return result
+  })
+}
+function forSlide({ current, next, inverted, layouts: { screen } }) {
+  const progress = Animated.add(
+    current.progress.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, 1],
+      extrapolate: 'clamp',
+    }),
+    next
+      ? next.progress.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, 1],
+          extrapolate: 'clamp',
+        })
+      : 0
+  )
+
+  return {
+    cardStyle: {
+      transform: [
+        {
+          translateX: Animated.multiply(
+            progress.interpolate({
+              inputRange: [0, 1, 2],
+              outputRange: [
+                screen.width, // Focused, but offscreen in the beginning
+                0, // Fully focused
+                screen.width * -0.3, // Fully unfocused
+              ],
+              extrapolate: 'clamp',
+            }),
+            inverted
+          ),
+        },
+      ],
+    },
+  }
+}
+
+export const createStackNavigatorWithDefaultScreenOptions = createNavigatorFactory(StackNavigator)
