28540e92bb699c9b6c800c37a61693926aa668dc
Dinu <84909896+Dinu222@users.noreply.github.com>
2024-01-13 21:58:28 +0200

    add unfinished ToastsProvider

--- src/app/_layout.js
+++ src/app/_layout.js
@@ -1,44 +1,43 @@
 import 'react-native-gesture-handler'
-import SplashView from '@components/SplashView/SplashView'
 import '@config/config'
+import SplashView from '@components/SplashView/SplashView'
 import NativeStackResponsableForScreenOrientation from '@libs/Navigation/NativeStackResponsableForScreenOrientation'
 import AppLoadingProvider, { useAppLoadingProvider } from '@providers/AppLoadingProvider'
 import FontsProvider from '@providers/FontsProvider'
 import ScreenOrientationProvider from '@providers/ScreenOrientationProvider'
 import SessionProvider from '@providers/SessionProvider'
+import ToastsProvider from '@providers/ToastsProvider'
 import TranslationProvider from '@providers/TranslationProvider'
 import { NativeWindStyleSheet } from 'nativewind'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import React from 'react'
-import { View } from 'react-native'
-import { SafeFullScreenLayout } from '@layouts/BaseLayout'
-import Writing from '@components/Writing/Writing'
-import Go from '@libs/Navigation/Go'
-
 
 //TODO add Localizations or Cronos objectx
 //TODO add buttons
+//TODO change splash view visibility prop name in dev mode hideInDev=true
+//TODO add lottie
 NativeWindStyleSheet.setOutput({ default: 'native' })
 
 export default function AppLayout() {
   return (
     <AppLoadingProvider>
-      <SafeAreaProvider>
-        <FontsProvider>
-          <ScreenOrientationProvider>
-            <TranslationProvider>
-              <SessionProvider>
-                <SplashView>
-                  <StopRenderIfAppNotLoaded>
-                    {/* Might change name */}
-                    <NativeStackResponsableForScreenOrientation />
-                  </StopRenderIfAppNotLoaded>
-                </SplashView>
-              </SessionProvider>
-            </TranslationProvider>
-          </ScreenOrientationProvider>
-        </FontsProvider>
-      </SafeAreaProvider>
+      <ToastsProvider>
+        <SafeAreaProvider>
+          <TranslationProvider>
+            <FontsProvider>
+              <ScreenOrientationProvider>
+                <SessionProvider>
+                  <SplashView>
+                    <StopRenderIfAppNotLoaded>
+                      {/* Might change name */}
+                      <NativeStackResponsableForScreenOrientation />
+                    </StopRenderIfAppNotLoaded>
+                  </SplashView>
+                </SessionProvider>
+              </ScreenOrientationProvider>
+            </FontsProvider>
+          </TranslationProvider>
+        </SafeAreaProvider>
+      </ToastsProvider>
     </AppLoadingProvider>
   )
 }
