00b85c712c4af64a6297dbe69332c1d880bfc34a
Dinu <84909896+Dinu222@users.noreply.github.com>
2024-02-25 21:18:54 +0200

    refactor

--- src/app/(app)/(auth)/(tab-bar)/search.js
+++ src/app/(app)/(auth)/(tab-bar)/search.js
@@ -8,13 +8,19 @@ import { FlatList } from 'react-native'
 const COLUMNS_COUNT = 2
 
 export default function Page() {
-  let columnOneItems = Food.Showcase.propsModels(100)
-  const { theme } = useAppTheme()
-  let itemsCount = 90
+  let items = Food.Showcase.propsModels(20)
+  return (
+    <LayoutForBottomTabs contentTw="flex-1" verticalPartBottomSpace={0}>
+      <SearchPanel />
+      <FoodList items={items} itemsCount={items.length} />
+    </LayoutForBottomTabs>
+  )
+}
+
+function FoodList({ items = [], itemsCount = 0 }) {
   const renderItem = useCallback(
     ({ item, index }) => {
-      // console.log(`props:`, props);
-      let isLast = index === 100 - 1
+      let isLast = index === items.length - 1
       let isFirst = index === 0
       let isNotFirstFromRightColumn = index !== 1
       let isLeftColumn = index % 2 === 0
@@ -23,6 +29,16 @@ export default function Page() {
       let spaceLeftColumn = 'pr-2 pl-1'
       let spaceRightColumn = 'pl-2 pr-1'
       let spaceVertical = 'pb-4'
+
+      function Placeholder() {
+        return (
+          <View
+            style={{
+              height: Algebra.percentageOf(LayoutForBottomTabs.defaultVerticalPartBottomSpace, 220),
+            }}
+          />
+        )
+      }
 
       return (
         <View
@@ -54,103 +70,47 @@ export default function Page() {
             ratingIconTw={'h-[14px] w-[14px]'}
             ratingContainerTw={'mt-4'}
           />
-          {isLast && (
-            <View
-              style={{
-                height: Algebra.percentageOf(
-                  LayoutForBottomTabs.defaultVerticalPartBottomSpace,
-                  200
-                ),
-              }}
-            ></View>
-          )}
+          {isLast && <Placeholder />}
         </View>
       )
     },
-    [itemsCount, columnOneItems]
+    [itemsCount, items]
   )
   return (
-    <LayoutForBottomTabs contentTw="flex-1" verticalPartBottomSpace={0}>
-      <SearchPanel />
-      <View tw={cn('relative mt-4')}>
-        <FaddingBaner background={theme.colors.background} />
-        <FlatList
-          style={{ backgroundColorX: 'blue', paddingTop: 30 }}
-          data={columnOneItems}
-          numColumns={COLUMNS_COUNT}
-          contentContainerStyle={{ columnGap: 20 }}
-          columnWrapperStyle={{
-            flex: 1,
-            width: '50%',
-          }}
-          windowSize={20}
-          // maxToRenderPerBatch={5}
-          keyExtractor={(_, index) => index}
-          renderItem={renderItem}
-          showsVerticalScrollIndicator={false}
-          bounces={false}
-        />
-      </View>
-    </LayoutForBottomTabs>
-  )
-}
-
-function renderItem({ item, index }) {
-  // console.log(`props:`, props);
-  let isLast = index === 100 - 1
-  let isFirst = index === 0
-  let isNotFirstFromRightColumn = index !== 1
-  let isLeftColumn = index % 2 === 0
-  let isRightColumn = !isLeftColumn
-
-  let spaceLeftColumn = 'pr-2 pl-1'
-  let spaceRightColumn = 'pl-2 pr-1'
-  let spaceVertical = 'pb-4'
-
-  return (
-    <View
-      tw={cn('w-full', {
-        [spaceVertical]: true,
-        [spaceLeftColumn]: isLeftColumn,
-        [spaceRightColumn]: isRightColumn,
-        '-mt-[104px]': isRightColumn && isNotFirstFromRightColumn,
-      })}
-    >
-      {isFirst && (
-        <Writing xl3 medium lineHeight={37} ctw={cn('mb-3 mt-0 self-start')}>
-          Found {'\n'}
-          {999} result
-        </Writing>
-      )}
-      <Food.Showcase
-        {...item}
-        elevation={1}
-        subtitleSize={{ xs: true }}
-        titleSize={{ lg: true }}
-        subtitleTw="mt-1"
-        titleMtTw="-mt-3"
-        containerTw="p-1"
-        currencySize={{ sm: true }}
-        priceSize={{ xl: true }}
-        priceContainerTw={'mt-4'}
-        valueTextSize={{ sm: true }}
-        ratingIconTw={'h-[14px] w-[14px]'}
-        ratingContainerTw={'mt-4'}
+    <View tw={cn('relative mt-4')}>
+      <FaddingBaner />
+      <FlatList
+        style={{
+          paddingTop: 30,
+        }}
+        data={items}
+        numColumns={COLUMNS_COUNT}
+        contentContainerStyle={{
+          columnGap: 20,
+        }}
+        columnWrapperStyle={{
+          flex: 1,
+          width: '50%',
+        }}
+        windowSize={10}
+        bouncesZoom={false}
+        alwaysBounceVertical={false}
+        overScrollMode="never"
+        keyExtractor={(_, index) => index}
+        renderItem={renderItem}
+        showsVerticalScrollIndicator={false}
       />
-      {isLast && (
-        <View
-          style={{
-            height: Algebra.percentageOf(LayoutForBottomTabs.defaultVerticalPartBottomSpace, 200),
-          }}
-        ></View>
-      )}
     </View>
   )
 }
-function FaddingBaner(props) {
+
+function FaddingBaner({ color }) {
+  const { theme } = useAppTheme()
+  if (!color) color = theme.colors.background
+
   return (
     <LinearGradient
-      colors={[props.background, 'transparent']}
+      colors={[color, 'transparent']}
       style={{
         position: 'absolute',
         top: 0,
