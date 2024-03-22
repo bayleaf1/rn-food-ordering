f7365b190effb6bd0deb27533c401fb1ed7d51cf
Bayleaf <152853920+bayleaf1@users.noreply.github.com>
2024-03-22 19:08:50 +0200

    small fixes

--- app.config.js
+++ app.config.js
@@ -17,6 +17,7 @@ export default ({ config }) => {
       assetBundlePatterns: ['**/*'],
       ios: {
         supportsTablet: true,
+        "bundleIdentifier": "com.anonymous.rn-pr-1"
       },
       android: {
         adaptiveIcon: {
