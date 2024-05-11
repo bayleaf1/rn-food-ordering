46dff7ca591aae3c459e42c80a6242bdddb8d549
Bayleaf <152853920+bayleaf1@users.noreply.github.com>
2024-05-11 19:23:43 +0300

    build for ios

--- app.config.js
+++ app.config.js
@@ -22,6 +22,7 @@ export default ({ config }) => {
       assetBundlePatterns: ['**/*'],
       ios: {
         supportsTablet: true,
+        bundleIdentifier: 'com.dinuciobanu.rn',
       },
       android: {
         adaptiveIcon: {
